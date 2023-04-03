199eb730f0d0d9537e62c01ffcecfc6a88b41d90
Denis Ivanchenko <sicknature24@yandex.com>
2023-04-03 13:50:42 +0300

    feat: add burger menu styles; update js functions

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -3,17 +3,27 @@
 const burgerIcon = document.querySelector('.burger');
 const headerNavbar = document.querySelector('.header__navbar');
 const main = document.querySelector('.main-page');
+const body = document.querySelector('body');
+const navbarLink = document.querySelectorAll('.navbar__link');
+
+function toggleBurgerAndHeader() {
+    body.classList.toggle('no-scroll');
+    headerNavbar.classList.toggle('header-active');
+    burgerIcon.classList.toggle('burger-opened');
+}
 
 burgerIcon.addEventListener('click', () => {
-    burgerIcon.classList.toggle('burger-opened');
-    headerNavbar.classList.toggle('header-active');
+    toggleBurgerAndHeader();
 })
 
 main.addEventListener('click', () => {
-    if(burgerIcon.classList.contains('burger-opened')) {
-        headerNavbar.classList.toggle('header-active');
-        burgerIcon.classList.toggle('burger-opened');
+    if (burgerIcon.classList.contains('burger-opened')) {
+        toggleBurgerAndHeader();
     }
+})
+
+navbarLink.forEach((element) => {
+    element.addEventListener('click', toggleBurgerAndHeader);
 })
 
 /*Burger handler ends*/
